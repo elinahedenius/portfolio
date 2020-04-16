eef2988feb41f5287e388f14b81e1b48056dfe65
elinahedenius <bohedenius@protonmail.com>
2020-04-17 00:02:58 +0200

    added a login thing

--- public/index.js
+++ public/index.js
@@ -1,3 +1,23 @@
  $(function(){
-   const socket = io.connect();
+   var socket = io.connect();
+
+   //login as a student
+   const $studentLogin = $('#studentLogin');
+   var $studentPsw = $('#studentPassword');
+   var $studentEmail = $('#studentEmail');
+   $studentLogin.submit(function(e){
+     socket.emit('student login', {email: $studentEmail.val(), password: $studentPsw.val()});
+     $studentPsw.val('');
+     $studentEmail.val('');
+   });
+
+   //login as a teacher
+   const $studentLogin = $('#teacherLogin');
+   var $studentPsw = $('#teacherPassword');
+   var $studentEmail = $('#teacherEmail');
+   $studentLogin.submit(function(e){
+     socket.emit('teacher login', {email: $teacherEmail.val(), password: $teacherPsw.val()});
+     $teacherPsw.val('');
+     $teacherEmail.val('');
+   });
  })
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const client = new Client({
   database: "testdb"
 });
 
+//connect to database
 client.connect((err) => {
   if (err) throw err;
   else{
@@ -19,5 +20,67 @@ client.connect((err) => {
   }
 });
 
+//start the server
+http.listen(3000, (err) => {
+  if (err) throw err;
+  else{
+    console.log('server started')
+  }
+})
+
+app.use(express.static(__dirname + '/public/'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html')
+})
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html')
+})
+
 client.query('select * from students')
-.then(results => console.table(results.rows))
+.then(res => console.log(res.rows))
+
+io.sockets.on('connection', (socket) => {
+
+  //student login
+  socket.on('student login', (data) => {
+    client.query('select * from students')
+    .then((res) => {
+      console.log(res.rows)
+      for(var i in res.rows){
+        if(data.email == res.rows[i].email){
+          if(data.password == res.rows[i].password){
+            console.log('sucess')
+          }
+          else{
+            console.log('wrong password')
+          }
+        }
+        else{
+          console.log('no uesr with that email')
+        }
+      }
+    })
+  })
+
+  //teacher login
+  socket.on('teacher login', (data) => {
+    client.query('select * from teachers')
+    .then((res) => {
+      console.log(res.rows)
+      for(var i in res.rows){
+        if(data.email == res.rows[i].email){
+          if(data.password == res.rows[i].password){
+            console.log('sucess')
+          }
+          else{
+            console.log('wrong password')
+          }
+        }
+        else{
+          console.log('no user with that email')
+        }
+      }
+    })
+  })
+
+})
